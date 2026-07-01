1e1cc8d0f8a1d394197b659762ac48418ef295b0
Mardakhay Ravinov <alfredrockankeur@gmail.com>
2026-07-01 10:38:08 +0400

    Export game card mapper

--- src/entities/game/index.ts
+++ src/entities/game/index.ts
@@ -1,6 +1,7 @@
 export { gameQueryKeys } from './api/queryKeys'
 export { gameService } from './api/gameService'
 export { useGameDetailsQuery, useGamesQuery } from './api/useGamesQuery'
+export { mapRawgGameToGameCard } from './lib/mapRawgGameToGameCard'
 export { GameCard, GameCardSkeleton } from './ui/GameCard'
 export type { GameCardGame } from './ui/GameCard'
 export type {
